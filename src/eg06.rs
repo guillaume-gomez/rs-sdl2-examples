5810d0c175ed2cc03ef5fba94a39a4f7d5143826
guillaume-gomez <guillaume.gomez846@gmail.com>
2016-01-21 00:03:08 +0100

    Work in progress : Window is closed thanks to window event

--- src/eg06.rs
+++ src/eg06.rs
@@ -6,23 +6,37 @@ fn main() {
     let ctx = sdl2::init().unwrap();
     let video_ctx = ctx.video().unwrap();
 
-    let window  = match video_ctx.window("eg06", 640, 480).position_centered().opengl().build() {
+    let first_window  = match video_ctx.window("eg06", 640, 480).position_centered().opengl().build() {
         Ok(window) => window,
         Err(err)   => panic!("failed to create window: {}", err)
     };
+
+    // let second_window  = match video_ctx.window("second_window", 640, 480).position_centered().opengl().build() {
+    //     Ok(window) => window,
+    //     Err(err)   => panic!("failed to create window: {}", err)
+    // };
+
     
-    let mut renderer = match window.renderer().build() {
-        Ok(renderer) => renderer,
-        Err(err) => panic!("failed to create renderer: {}", err)
-    };
+    let mut renderer = match first_window.renderer().build() {
+         Ok(renderer) => renderer,
+         Err(err) => panic!("failed to create renderer: {}", err)
+     };
     let _ = renderer.present();
 
     let mut events = ctx.event_pump().unwrap();
     // loop until we receive a QuitEvent
     'event : loop {
         for event in events.poll_iter() {
+            //println!("{:?}",event.Window.window_id );
             match event {
-                Event::Quit{..} => break 'event,
+                Event::Window{window_id: truc,  win_event_id: window, ..} => 
+                    {
+                        println!("{:?} ==> {:?}", truc, window );
+                        match window {
+                            sdl2::event::WindowEventId::Close => break 'event,
+                            _ => continue,
+                        }
+                    },
                 _               => continue
             }
         }
