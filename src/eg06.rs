3d00aff9e1f3d0217ff354c64f8eefadeb2bba16
Guillaume Gomez <guillaume.gomez846@gmail.com>
2016-01-31 22:20:46 +0100

    This example is now functional. Next steps :  - remove commented lines  - randomize new window's size  - insert a renderer

--- src/eg06.rs
+++ src/eg06.rs
@@ -1,22 +1,27 @@
 extern crate sdl2;
 
 use sdl2::event::{Event};
+use sdl2::video::{Window};
+use sdl2::keyboard::{Keycode};
+use sdl2::{VideoSubsystem};
+
+
+fn build_window(video_ctx: &VideoSubsystem, title: &str) -> (Window) {
+    let window = match video_ctx.window(title, 640, 480).position_centered().opengl().build() {
+        Ok(window) => window,
+        Err(err)   => panic!("failed to create window: {}", err)
+    };
+    return window;
+}
 
 fn main() {
     let ctx = sdl2::init().unwrap();
     let video_ctx = ctx.video().unwrap();
 
-    let first_window  = match video_ctx.window("eg06", 640, 480).position_centered().opengl().build() {
-        Ok(window) => window,
-        Err(err)   => panic!("failed to create window: {}", err)
-    };
-
-    // let second_window  = match video_ctx.window("second_window", 640, 480).position_centered().opengl().build() {
-    //     Ok(window) => window,
-    //     Err(err)   => panic!("failed to create window: {}", err)
-    // };
-
+    let mut window_ids_vec = Vec::new();
     
+    window_ids_vec.push(build_window(&video_ctx, "first_window"));
+       
     // let mut renderer = match first_window.renderer().build() {
     //      Ok(renderer) => renderer,
     //      Err(err) => panic!("failed to create renderer: {}", err)
@@ -24,8 +29,6 @@ fn main() {
     // let _ = renderer.present();
 
     let mut events = ctx.event_pump().unwrap();
-    let id  :&u32 = &first_window.id();
-    let mut window_ids_vec = vec![id];
     // loop until we receive a QuitEvent 
     while window_ids_vec.len() > 0 {
         for event in events.poll_iter() {
@@ -34,10 +37,15 @@ fn main() {
                     {
                         println!("{:?} ==> {:?}", window_id, window_event_id );
                         match window_event_id {
-                            sdl2::event::WindowEventId::Close =>  window_ids_vec.retain(|&id| *id != window_id),
+                            sdl2::event::WindowEventId::Close =>  
+                            { 
+                                window_ids_vec.retain(|window| window.id() != window_id)
+                            },
                             _ => continue,
                         }
                     },
+                Event::KeyDown { keycode: Some(Keycode::N), .. } =>  
+                    window_ids_vec.push(build_window(&video_ctx, "another window")) ,
                 _               => continue
             }
         }
